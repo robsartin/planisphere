6e5fa98f41b0d26c96483335e627a334217e8452
robsartin <98853091+robsartin@users.noreply.github.com>
2026-04-15 18:16:53 -0500

    chore: add ESLint + Prettier with typescript-eslint type-checked rules (#17)

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaVersion: 2022,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended-type-checked", "prettier"],
+  env: { browser: true, es2022: true, node: true },
+  ignorePatterns: ["dist/", "coverage/", ".worktrees/", "*.cjs"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    eqeqeq: ["error", "always"],
+  },
+};
