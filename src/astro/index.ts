af152b3b83175595dee382ddc151fd5445aeccdd
robsartin <98853091+robsartin@users.noreply.github.com>
2026-04-16 18:49:13 -0500

    feat(astro): add barrel export (#48)

---
+++ src/astro/index.ts
@@ -0,0 +1,5 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+export { type StarRecord, type CatalogLoadError, parseCatalog } from "./catalog";
+export { type HorizontalCoord, raDecToAltAz } from "./coords";
+export { type StarVisual, magToVisual } from "./magnitude";
+export { type AltAzStar, filterVisibleStars } from "./visibility";
