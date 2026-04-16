1f08e293112f162b71c0fade492bb5f4516631f2
Rob Sartin <rob.sartin@gmail.com>
2026-04-15 20:14:13 -0500

    feat(state): add URL-synced AppState (observer + timeUtc) with Result-based parser

---
+++ src/state/index.ts
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+export {
+  type AppState,
+  type Observer,
+  type StateParseError,
+  DEFAULT_STATE,
+  parseStateFromSearchParams,
+  serializeStateToSearchParams,
+} from "./state";
